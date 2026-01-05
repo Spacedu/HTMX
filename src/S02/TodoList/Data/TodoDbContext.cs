5e542c9a63763af7da8be554f3499798fcf7ddf2
Spacedu <contato@spacedu.com.br>
2026-01-05 15:34:16 -0300

    S01E07-08 - Configurar Banco de dados

---
+++ src/S02/TodoList/Data/TodoDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
+namespace TodoList.Data;
+public class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<User> Users {get;set;}
+    public DbSet<TodoTask> TodoTasks {get;set;}
+}
--- src/S02/TodoList/Program.cs
+++ src/S02/TodoList/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<TodoDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
