b999cf15ae2c94763d4ab8ed2c514055b1f314ea
Spacedu <contato@spacedu.com.br>
2026-01-05 12:55:57 -0300

    S02E02 - Projeto criado

---
+++ src/S02/TodoList/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
